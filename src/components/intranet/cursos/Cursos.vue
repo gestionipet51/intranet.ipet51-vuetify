d6440f0c63f9bc70393f90c488ffdcdf8c5e30b6
gestion.ipet51@gmail.com <7gy483rte38ssxtgcy1yufiib1slia@bots.bitbucket.org>
2024-05-15 11:26:33 -0300

    GSF : 15-05-24

--- src/components/intranet/cursos/Cursos.vue
+++ src/components/intranet/cursos/Cursos.vue
@@ -79,6 +79,12 @@ export default {
             dialog:false,
             dialogDelete:false,
             editedIndex: -1,
+            editedItem:{
+                id:0,
+                ciclo:'',
+                anio:0,
+                division:''
+            }
         }),
         methods:{
             initialize(){
@@ -120,20 +126,45 @@ export default {
         <v-app-bar :elevation="2" title="Cursos" color="deep-purple-darken-4"></v-app-bar>
 
         <v-row no-gutters>
-          <v-col></v-col>
-          <v-col>
+          <v-col cols="3"></v-col>
+          <v-col cols="6">
             <v-sheet class="pa-2 ma-2">
-              <v-card title="Buscador" flat>
+              <v-card title="Crud - Cursos" flat>
                 <template v-slot:text>
-                  <v-text-field
-                          v-model="search"
-                          label="Buscar"
-                          prepend-inner-icon="mdi-magnify"
-                          variant="outlined"
-                          hide-details
-                          single-line
-                  >
-                  </v-text-field>
+                  <v-toolbar flat>
+                        <v-text-field
+                                    v-model="search"
+                                    label="Buscar"
+                                    prepend-inner-icon="mdi-magnify"
+                                    variant="outlined"
+                                    hide-details
+                                    single-line
+                                    cols="2"
+                        >
+                        </v-text-field>
+                        <v-dialog v-model="dialog" max-width="500px">
+                            <template v-slot:activator="{ props }">
+                                <v-btn class="mb-2" color="primary" variant="elevated" v-bind="props"> Nuevo Curso</v-btn>
+                            </template>
+                            <v-card>
+                                <v-card-title>
+                                        <span class="text-h5"> {{ form-title }}</span>
+                                </v-card-title>
+                                <v-card-text>
+                                    <v-container>
+                                        <v-row>
+                                            <v-col cols="12" md="4" sm="6">
+                                                <v-text-field v-model="editedItem.id" label="Id"> </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" md="4" sm="6"> 
+                                                <v-text-field v-model="editedItem.anio" label="Año"></v-text-field> 
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+                            </v-card>
+                        </v-dialog>
+                  </v-toolbar>
                 </template>
                 <v-data-table-server 
                 v-model:items-per-page="itemsPerPage"
@@ -147,18 +178,10 @@ export default {
                 :single-select="singleSelect"
                 >
                 <template v-slot:item.actions="{ item }">
-                  <v-icon
-                    class="me-2"
-                    size="small"
-                    @click="editItem(item)"
-                  >
-                    mdi-pencil
+                  <v-icon class="me-2" size="small" @click="editItem(item)">mdi-pencil
                   </v-icon>
                   <v-icon
-                    size="small"
-                    @click="deleteItem(item)"
-                  >
-                    mdi-delete
+                    size="small"   @click="deleteItem(item)" >mdi-delete
                   </v-icon>
                 </template>
               </v-data-table-server>
