2a49ee3c18c0d52eac4357d2147fcc7a5ac9b483
gestion.ipet51@gmail.com <8d9omwtiwm87h9ibb284qtux58g1vh@bots.bitbucket.org>
2024-11-26 07:09:45 -0300

    GSF 26-11-2024: Se incorpora lista de matricula

--- src/components/intranet/hojaderuta/HojaDeRuta.vue
+++ src/components/intranet/hojaderuta/HojaDeRuta.vue
@@ -5,9 +5,25 @@
             <v-toolbar-title class="ml-2 text-h6 text-md-h5">Hoja de Ruta</v-toolbar-title>
         </v-app-bar>
 
+        <!--v-row>
+            
+                <v-col cols="6">
+                    <v-file-input  accept=".csv" label="Subir CSV a Firebase" @change="procesarArchivo"></v-file-input>
+                </v-col>
+                    <v-col cols="3">
+                        <v-btn :disabled="!datosCSV.length" rounded="lg" @click="enviarDatos" color="indigo-darken-3">
+                            Subir Archivo 
+                        </v-btn>
+                </v-col>
+        </v-row-->
+
         <v-row>
             <v-col cols="2"></v-col>
-            <v-col cols="6"><v-select label="Seleccione" :items="cursos" :item-props="cursosProps"></v-select></v-col>
+            <v-col cols="6"><v-select label="Seleccione" :items="cursos" item-title="descripcion" item-value ="id"></v-select></v-col>
+        </v-row>
+
+        <v-row>
+            
         </v-row>
     </v-container>
 
@@ -15,20 +31,20 @@
 
 
 <script>
-    const vCursos=[ {id:1,tag:"Primer Año A",anio:1,division:"A", ciclo:"PC"},
-                    {id:2,tag:"Primer Año B",anio:1,division:"B", ciclo:"PC"},
-                    {id:3,tag:"Primer Año C",anio:1,division:"C", ciclo:"PC"},
-                    {id:4,tag:"Primer Año D",anio:1,division:"D", ciclo:"PC"}];
-
 
     import { db } from '../../../firebaseConfig';
     import { collection ,addDoc,getDocs,doc,deleteDoc,updateDoc, getPersistentCacheIndexManager } from 'firebase/firestore';
+
+    import Papa from "papaparse"; // Importar PapaParse para procesar CSV
+    import axios from "axios"; // Axios para enviar datos al backend    
+
 
     export default {
         data:()=>{
             return {
                 cursos:'',
                 loading:false,
+                datosCSV:[],
             }
         },
         methods:{
@@ -37,6 +53,7 @@
                 // this.ciclos = myCiclos;
                 this.loading = true;
                 await this.fetchCursos();
+                console.log(this.cursos);
              },
             fetchCursos: async function (){
                 const querySnapshot = await getDocs(collection(db,"cursos"));
@@ -53,7 +70,36 @@
                     anio : item.anio,
                     descripcion: item.descripcion,
                 }
-            }
+            },
+            procesarArchivo(event) {
+                const archivo = event.target.files[0];
+                if (archivo) {
+                    Papa.parse(archivo, {
+                    header: true, // Usa la primera fila como encabezado
+                    complete: (resultados) => {
+                        this.datosCSV = resultados.data; // Guardar los datos procesados
+                        console.log("Datos procesados:", this.datosCSV);
+                    },
+                    error: (error) => {
+                        console.error("Error al procesar el archivo:", error);
+                    }
+                    });
+                }
+            },
+            enviarDatos() {
+                try {
+                    // const respuesta = await axios.post("http://localhost:3000/subir-csv", this.datosCSV);
+                    this.datosCSV.forEach((elemento) => { 
+                            // addDoc(collection(db,"matricula"),elemento);  
+                            console.log("=>" + elemento.idhex );
+                        
+                    }) 
+                    alert("Datos subidos con éxito: " + respuesta.data.message);
+                } catch (error) {
+                    console.error("Error al subir datos:", error);
+                    alert("Error al subir los datos." );
+                }
+            },
         },
         created(){
             this.initialize();
