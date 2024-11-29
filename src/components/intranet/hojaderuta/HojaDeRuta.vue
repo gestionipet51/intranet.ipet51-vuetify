a399a8bcc6e27caadf5a0feb410f28013f987d88
gestion.ipet51@gmail.com <8d9omwtiwm87h9ibb284qtux58g1vh@bots.bitbucket.org>
2024-11-29 07:05:47 -0300

    GSF 29112024 - Se incluye modales

--- src/components/intranet/hojaderuta/HojaDeRuta.vue
+++ src/components/intranet/hojaderuta/HojaDeRuta.vue
@@ -4,8 +4,8 @@
             <v-app-bar-nav-icon icon="mdi-playlist-edit"></v-app-bar-nav-icon>
             <v-toolbar-title class="ml-2 text-h6 text-md-h5">Hoja de Ruta</v-toolbar-title>
         </v-app-bar>
-        <!--
-            <v-row>
+        
+            <v-row v-if="loadfile == true ">
                 
                     <v-col cols="6">
                         <v-file-input  accept=".csv" label="Subir CSV a Firebase" @change="procesarArchivo"></v-file-input>
@@ -16,7 +16,7 @@
                             </v-btn>
                     </v-col>
                 </v-row>
-        -->
+        
 
         <v-row>
             <v-col cols="2"></v-col>
@@ -46,18 +46,78 @@
 
                         <v-dialog v-model="dialogFactory" max-width="500px">
                             <v-card>
-                                <v-card-title class="text-h5">Hoja de Ruta: Registro de Taller </v-card-title>
+                                <v-card-title class="text-h5">{{ itemSelected.apellido }} , {{ itemSelected.nombre }} : Hoja de Ruta - Registro de Taller </v-card-title>
                                 <v-card-text>
-                                    
+                                    <v-row dense>
+                                        <v-col cols="12" md="6" sm="6">
+                                            <v-select
+                                                label="Responsable"
+                                                :items="tlResponsables"
+                                                item-title="nombre" 
+                                                item-value ="id"        
+                                                v-model="itemSelected.tl_responsable"
+                                                required
+                                            ></v-select>
+                                        </v-col>
+
+                                        <v-col cols="12" md="6" sm="6">
+                                            <v-select
+                                                label="Condición"
+                                                :items="opEstados"
+                                                item-title="tag" 
+                                                item-value ="id"        
+                                                v-model="itemSelected.tl_condicion"
+                                                required
+                                            ></v-select>
+                                        </v-col>
+                                    </v-row>
                                 </v-card-text>
                                 <v-card-actions>
                                     <v-spacer></v-spacer>
-                                    <v-btn color="blue-darken-1" variant="text" @click="closeFactory">Cancel</v-btn>
-                                    <v-btn color="blue-darken-1" variant="text" @click="updateFactory">OK</v-btn>
+                                        <v-btn color="red-accent-4" variant="text" @click="closeFactory" icon="mdi-arrow-u-left-top-bold"></v-btn>
+                                        <v-btn color="blue-darken-4" variant="text" @click="updateFactory" icon="mdi-content-save-settings"></v-btn>
                                     <v-spacer></v-spacer>
                                 </v-card-actions>
                             </v-card>
                             </v-dialog>
+
+                            <v-dialog v-model="dialogBiblioteca" max-width="600px">
+                            <v-card>
+                                <v-card-title class="text-h5">{{ itemSelected.apellido }} , {{ itemSelected.nombre }} : Hoja de Ruta - Registro de Biblioteca</v-card-title>
+                                <v-card-text>
+                                    <v-row dense>
+                                        <v-col cols="12" md="6" sm="6">
+                                            <v-select
+                                                label="Responsable"
+                                                :items="blResponsables"
+                                                item-title="nombre" 
+                                                item-value ="id"        
+                                                v-model="itemSelected.bl_responsable"
+                                                required
+                                            ></v-select>
+                                        </v-col>
+
+                                        <v-col cols="12" md="6" sm="6">
+                                            <v-select
+                                                label="Condición"
+                                                :items="opEstados"
+                                                item-title="tag" 
+                                                item-value ="id"        
+                                                v-model="itemSelected.bl_condicion"
+                                                required
+                                            ></v-select>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                        <v-btn color="red-accent-4" variant="text" @click="closeFactory" icon="mdi-arrow-u-left-top-bold"></v-btn>
+                                        <v-btn color="blue-darken-4" variant="text" @click="updateFactory" icon="mdi-content-save-settings"></v-btn>
+                                    <v-spacer></v-spacer>
+                                </v-card-actions>
+                            </v-card>
+                            </v-dialog>
+
                     </v-toolbar>
                 </template>
                 <template v-slot:item.actions="{ item }">
@@ -73,7 +133,7 @@
                         class="me-2"
                         size="small"
                         color="teal-darken-3"
-                        @click="editLibrary(item)"
+                        @click="editBiblioteca(item)"
                     >
                         mdi-library
                     </v-icon>
@@ -81,7 +141,7 @@
                         class="me-2"
                         size="small"
                         color="red-accent-4"
-                        @click="editPaid(item)"
+                        @click="editCooperadora(item)"
                         >
                         mdi-currency-usd
                     </v-icon>
@@ -89,7 +149,7 @@
                             class="me-2"
                             size="small"
                             color="deep-purple-accent-3"
-                            @click="editBedroom(item)"
+                            @click="editInternado(item)"
                             >
                             mdi-bed-single-outline
                     </v-icon>
@@ -110,10 +170,10 @@
     import Papa from "papaparse"; // Importar PapaParse para procesar CSV
     import axios from "axios"; // Axios para enviar datos al backend    
 
-    /*
-    const vCondicion = [{id:1,tag:"Completo",key:"COMPLETO"},{id:2,tag:"Pendiente",key:"PENDIENTE"},{id:3,tag:"NO CORRESPONDE",key:"NC"}];
+    const vEstados = [{id:1,tag:"Completo",key:"COMPLETO"},{id:2,tag:"Pendiente",key:"PENDIENTE"},{id:3,tag:"NO CORRESPONDE",key:"NC"}];
 
-    const vAutorizante = [{id:1,grupo:"COOPERADORA",nombre:"PERALTA,FLORENCIA"},
+    
+    const vResponsables = [{id:1,grupo:"COOPERADORA",nombre:"PERALTA,FLORENCIA"},
                           {id:2,grupo:"TALLER",nombre:"LAZZARO MATAR,CARLOS"},
                           {id:3,grupo:"TALLER",nombre:"GOROCITO,WALTER"},
                           {id:4,grupo:"TALLER",nombre:"VICENTE,JORGE"},
@@ -127,7 +187,7 @@
                           {id:12,grupo:"INTERNADO",nombre:"MARCANTONIO;GERMAN"},
                           {id:13,grupo:"INTERNADO",nombre:"LAMBERTUCCI,FRANCISCO"},
                         ];
-    */
+    
     export default {
         data(){
             return {
@@ -142,6 +202,13 @@
                 dialogLibrary:false,
                 dialogPaid:false,
                 dialogBedroom:false,
+                loadfile: false,
+                opEstados:[],
+                tlResponsables:[],
+                cpResponsables:[],
+                blResponsables:[],
+                intResponsables:[],
+                itemSelected:{},
                 headers:[
                     { title:'Id',align:'start',sortable:false,key:'id' },
                     { title:'IdHex',align:'center',sortable:false,key:'idhex'},
@@ -152,9 +219,9 @@
                     // { title:'Cod.Plan Est.',align:'left',sortable:false,key:'Código Plan de Estudio'},
                     // { title:'Plan Estudio',align:'left',sortable:false,key:'Plan de Estudio'},
                     { title:'Orden',align:'center',sortable:false,key:'orden'},
-                    { title:'Nro.Doc.',align:'center',sortable:false,key:'Nro. Documento'},
-                    { title:'Apellido',align:'left',sortable:true,key:'Apellido'},
-                    { title:'Nombres',align:'left',sortable:false,key:'Nombre'},
+                    { title:'Nro.Doc.',align:'center',sortable:false,key:'nro_documento'},
+                    { title:'Apellido',align:'left',sortable:true,key:'apellido'},
+                    { title:'Nombres',align:'left',sortable:false,key:'nombre'},
                     { title: 'Actions', key: 'actions', sortable: false },
                 ]
 
@@ -167,6 +234,12 @@
                 this.loading = true;
                 await this.fetchCursos();
                 await this.fetchMatriculas();
+                this.opEstados = vEstados;
+                this.tlResponsables = vResponsables.filter(elem => elem.grupo == "TALLER");
+                this.cpResponsables = vResponsables.filter(elem => elem.grupo == "COOPERADORA");
+                this.blResponsables = vResponsables.filter(elem => elem.grupo == "BIBLIOTECA");
+                this.intResponsables = vResponsables.filter(elem => elem.grupo == "INTERNADO");
+
                 console.log(this.matriculas);
              },
             fetchCursos: async function (){
@@ -177,7 +250,7 @@
                 this.loading = false;
             },
             fetchMatriculas:async function(){
-                const qrySnapshot = await getDocs(collection(db,"matricula"));
+                const qrySnapshot = await getDocs(collection(db,"matriculas"));
                 this.matriculas = qrySnapshot.docs
                                  .map(doc => ({id:doc.id ,...doc.data()}));
                 this.loading = false;
@@ -210,7 +283,7 @@
                 try {
                     // const respuesta = await axios.post("http://localhost:3000/subir-csv", this.datosCSV);
                     this.datosCSV.forEach((elemento) => { 
-                            // addDoc(collection(db,"matricula"),elemento);  
+                            addDoc(collection(db,"matriculas"),elemento);  
                             console.log("=>" + elemento.idhex );
                         
                     }) 
@@ -235,11 +308,37 @@
             },
             editFactory(item){
                 console.log(item);
+                this.itemSelected = item;
                 this.dialogFactory = true;
 
             },
             closeFactory(){
+                this.itemSelected = null;
                 this.dialogFactory = false;
+            },
+            editBiblioteca(item){
+                this.itemSelected = item;
+                this.dialogBiblioteca = true;
+            },
+            closeBiblioteca(){
+                this.itemSelected = null;
+                this.dialogBiblioteca = false;
+            },
+            editCooperadora(item){
+                this.itemSelected = item;
+                this.dialogCooperadora = true;
+            },
+            closeCooperadora(){
+                this.itemSelected = null;
+                this.dialogCooperadora = false;
+            },
+            editInternado(item){
+                this.itemSelected = item;
+                this.dialogInternado = true;
+            },
+            closeInternado(){
+                this.itemSelected = null;
+                this.dialogInternado = false;
             }
         },
         created(){
