7d68bd1f9144e75c69686ebc310ebf5fb7dc1fb6
gestion.ipet51@gmail.com <8d9omwtiwm87h9ibb284qtux58g1vh@bots.bitbucket.org>
2025-05-08 11:17:06 -0300

    GSF 08052025

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /**
  * main.js
  *
- * Bootstraps Vuetify and other plugins then mounts the App`
+ * Bootstraps Vuetify and other plugins then mounts the App
  */
 
 // Plugins
@@ -15,20 +15,26 @@ import { createApp } from 'vue'
 
 import router from './router';
 
+/*
 import GAuth from "vue3-google-oauth2"
-
 
 const gAuthOptions = {
     clientId: "911892646464-2hcu7egk7dkk2kl8u7glk7b8gt8ojtpu.apps.googleusercontent.com",
     scope: "https://www.googleapis.com/auth/documents.readonly",
     prompt: "consent",
   };
+* */
 
+
+import GoogleSignInPlugin from "vue3-google-signin"
+const gOptions =  {
+    clientId: 'CLIENT ID OBTAINED FROM GOOGLE API CONSOLE',
+  };
 
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.use(router);
-app.use(GAuth,gAuthOptions);
+app.use(GoogleSignInPlugin,gOptions);
 app.mount('#app');
